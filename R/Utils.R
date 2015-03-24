ec1090630fd52cef32b28e4408543d25b589d79e
unknown <mq20111250@IT-3102.mqauth.uni.mq.edu.au>
2015-03-24 15:58:25 +1100

    Move functions into utils file, move packages into remake.yml

--- R/Utils.R
+++ R/Utils.R
@@ -1,7 +1,20 @@
 
+read_csv <- function(filename) {
+  read.csv(filename, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+}
+
 tex_2_pdf <- function(texfile){
   filename <- tools::file_path_sans_ext(texfile)
   system(sprintf("pdflatex %s", texfile))
   aux.files <- paste0(filename, c(".log", ".aux", ".bbl", ".blg"))
   file.remove(aux.files[file.exists(aux.files)])
+}
+
+#define functions
+se <- function(x) {
+  sd(x)/sqrt(length(x))
+}
+
+con95 <- function(x){
+  (sd(x)/sqrt(length(x)))*1.96
 }
