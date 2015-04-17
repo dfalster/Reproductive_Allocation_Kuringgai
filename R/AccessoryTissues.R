dfalster/Reproductive_Allocation_Kuringgai

commit 9d4ddf53927a86295dfa608566e412c7659acd9f

get ages from individual list rather than harvest. Also close #6, and item 3 in #4

R/AccessoryTissues.R
@@ -1,15 +1,13 @@
-IAT_Species <- function(species, Reproduction, HarvestData, InvestmentCategories, species_INV) {
+IAT_Species <- function(species, IndividualsList, InvestmentCategories, species_INV) {
   # Read and restrict the data to the subset of interest.
 
-  # TODO :get ages from individual list, not from harvest data
-  AgeData <- unique(
-    filter(HarvestData, segment == 1, plant_status == "alive", individual %in% Reproduction$individual) %>%
-    select(age, individual)
-      )
+  AgeData <- IndividualsList %>%
+    filter(use_for_allocation_calculations & alive) %>%
+    select(individual, age)
 
   InvDist <- data.frame(species=c(), individual = c(), PrePol_A = c(), PrePol_S = c(), PostPol_A = c(), PD = c(), Prop = c(), Total = c(), age = c(), ageExact = c())
 
-  for (individual in unique(Reproduction$individual)) {
+  for (individual in unique(IndividualsList$individual)) {
     # print(individual)
     Ind <- InvestmentInAccessoryTissues(individual, species, InvestmentCategories[, c("flower_part", species)], species_INV)
     if (length(AgeData[AgeData[, 2] == individual, 1]) == 1) {

R/ReproductionAllocation.R
@@ -1,6 +1,5 @@
 RA_Calculations <- function(thisSpecies, Species_Investment, HarvestData, Maps, IndividualsList) {
 
-
   ### Calculate Regression coefficients based on common slope and intercept by the basal diameter at year 2013
 
   WD.species <- CalculateMassAndDiameters(HarvestData)
@@ -16,7 +15,7 @@ RA_Calculations <- function(thisSpecies, Species_Investment, HarvestData, Maps, 
   # Use only basal diameter of the trees that are alive and have status use.
   DiameterData <- HarvestData %>%
     filter(individual %in% IndividualsList$individual[IndividualsList$use_for_allocation_calculations]) %>%
-    filter(segment == 1, use_status == "use", plant_status == "alive")  %>%
+    filter(segment == 1)  %>%
     select(individual, age, start_end, diameter_1, diameter_2, diameter_3, total_plant_weight)
 
   for (i in seq_len(nrow(DiameterData))) {

maker_species_data.yml
@@ -332,11 +332,12 @@ targets:
 
   BAER_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForBAER(BAER_ReproductionData, BAER_HarvestData, InvestmentCategories, BAER_Investment)
+      CalculateAccesoryCostsForBAER(BAER_IndividualsList, InvestmentCategories, BAER_Investment)
 
   BAER_ReproductionAllocation:
     command: >
-      CalculateRAForBAER(BAER_Investment, BAER_HarvestData, BAER_GraphMaps, IndividualsList)
+      CalculateRAForBAER(BAER_Investment,  BAER_HarvestData, BAER_GraphMaps, BAER_IndividualsList)
+
   BOLE_IndividualsList:
     command: filterToBOLE(IndividualsList)
 
@@ -364,11 +365,12 @@ targets:
 
   BOLE_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForBOLE(BOLE_ReproductionData, BOLE_HarvestData, InvestmentCategories, BOLE_Investment)
+      CalculateAccesoryCostsForBOLE(BOLE_IndividualsList, InvestmentCategories, BOLE_Investment)
 
   BOLE_ReproductionAllocation:
     command: >
-      CalculateRAForBOLE(BOLE_Investment, BOLE_HarvestData, BOLE_GraphMaps, IndividualsList)
+      CalculateRAForBOLE(BOLE_Investment,  BOLE_HarvestData, BOLE_GraphMaps, BOLE_IndividualsList)
+
   COER_IndividualsList:
     command: filterToCOER(IndividualsList)
 
@@ -396,11 +398,12 @@ targets:
 
   COER_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForCOER(COER_ReproductionData, COER_HarvestData, InvestmentCategories, COER_Investment)
+      CalculateAccesoryCostsForCOER(COER_IndividualsList, InvestmentCategories, COER_Investment)
 
   COER_ReproductionAllocation:
     command: >
-      CalculateRAForCOER(COER_Investment, COER_HarvestData, COER_GraphMaps, IndividualsList)
+      CalculateRAForCOER(COER_Investment,  COER_HarvestData, COER_GraphMaps, COER_IndividualsList)
+
   EPMI_IndividualsList:
     command: filterToEPMI(IndividualsList)
 
@@ -428,11 +431,12 @@ targets:
 
   EPMI_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForEPMI(EPMI_ReproductionData, EPMI_HarvestData, InvestmentCategories, EPMI_Investment)
+      CalculateAccesoryCostsForEPMI(EPMI_IndividualsList, InvestmentCategories, EPMI_Investment)
 
   EPMI_ReproductionAllocation:
     command: >
-      CalculateRAForEPMI(EPMI_Investment, EPMI_HarvestData, EPMI_GraphMaps, IndividualsList)
+      CalculateRAForEPMI(EPMI_Investment,  EPMI_HarvestData, EPMI_GraphMaps, EPMI_IndividualsList)
+
   GRBU_IndividualsList:
     command: filterToGRBU(IndividualsList)
 
@@ -460,11 +464,12 @@ targets:
 
   GRBU_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForGRBU(GRBU_ReproductionData, GRBU_HarvestData, InvestmentCategories, GRBU_Investment)
+      CalculateAccesoryCostsForGRBU(GRBU_IndividualsList, InvestmentCategories, GRBU_Investment)
 
   GRBU_ReproductionAllocation:
     command: >
-      CalculateRAForGRBU(GRBU_Investment, GRBU_HarvestData, GRBU_GraphMaps, IndividualsList)
+      CalculateRAForGRBU(GRBU_Investment,  GRBU_HarvestData, GRBU_GraphMaps, GRBU_IndividualsList)
+
   GRSP_IndividualsList:
     command: filterToGRSP(IndividualsList)
 
@@ -492,11 +497,12 @@ targets:
 
   GRSP_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForGRSP(GRSP_ReproductionData, GRSP_HarvestData, InvestmentCategories, GRSP_Investment)
+      CalculateAccesoryCostsForGRSP(GRSP_IndividualsList, InvestmentCategories, GRSP_Investment)
 
   GRSP_ReproductionAllocation:
     command: >
-      CalculateRAForGRSP(GRSP_Investment, GRSP_HarvestData, GRSP_GraphMaps, IndividualsList)
+      CalculateRAForGRSP(GRSP_Investment,  GRSP_HarvestData, GRSP_GraphMaps, GRSP_IndividualsList)
+
   HATE_IndividualsList:
     command: filterToHATE(IndividualsList)
 
@@ -524,11 +530,12 @@ targets:
 
   HATE_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForHATE(HATE_ReproductionData, HATE_HarvestData, InvestmentCategories, HATE_Investment)
+      CalculateAccesoryCostsForHATE(HATE_IndividualsList, InvestmentCategories, HATE_Investment)
 
   HATE_ReproductionAllocation:
     command: >
-      CalculateRAForHATE(HATE_Investment, HATE_HarvestData, HATE_GraphMaps, IndividualsList)
+      CalculateRAForHATE(HATE_Investment,  HATE_HarvestData, HATE_GraphMaps, HATE_IndividualsList)
+
   HEPU_IndividualsList:
     command: filterToHEPU(IndividualsList)
 
@@ -556,11 +563,12 @@ targets:
 
   HEPU_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForHEPU(HEPU_ReproductionData, HEPU_HarvestData, InvestmentCategories, HEPU_Investment)
+      CalculateAccesoryCostsForHEPU(HEPU_IndividualsList, InvestmentCategories, HEPU_Investment)
 
   HEPU_ReproductionAllocation:
     command: >
-      CalculateRAForHEPU(HEPU_Investment, HEPU_HarvestData, HEPU_GraphMaps, IndividualsList)
+      CalculateRAForHEPU(HEPU_Investment,  HEPU_HarvestData, HEPU_GraphMaps, HEPU_IndividualsList)
+
   LEES_IndividualsList:
     command: filterToLEES(IndividualsList)
 
@@ -588,11 +596,12 @@ targets:
 
   LEES_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForLEES(LEES_ReproductionData, LEES_HarvestData, InvestmentCategories, LEES_Investment)
+      CalculateAccesoryCostsForLEES(LEES_IndividualsList, InvestmentCategories, LEES_Investment)
 
   LEES_ReproductionAllocation:
     command: >
-      CalculateRAForLEES(LEES_Investment, LEES_HarvestData, LEES_GraphMaps, IndividualsList)
+      CalculateRAForLEES(LEES_Investment,  LEES_HarvestData, LEES_GraphMaps, LEES_IndividualsList)
+
   PELA_IndividualsList:
     command: filterToPELA(IndividualsList)
 
@@ -620,11 +629,12 @@ targets:
 
   PELA_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForPELA(PELA_ReproductionData, PELA_HarvestData, InvestmentCategories, PELA_Investment)
+      CalculateAccesoryCostsForPELA(PELA_IndividualsList, InvestmentCategories, PELA_Investment)
 
   PELA_ReproductionAllocation:
     command: >
-      CalculateRAForPELA(PELA_Investment, PELA_HarvestData, PELA_GraphMaps, IndividualsList)
+      CalculateRAForPELA(PELA_Investment,  PELA_HarvestData, PELA_GraphMaps, PELA_IndividualsList)
+
   PEPU_IndividualsList:
     command: filterToPEPU(IndividualsList)
 
@@ -652,11 +662,12 @@ targets:
 
   PEPU_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForPEPU(PEPU_ReproductionData, PEPU_HarvestData, InvestmentCategories, PEPU_Investment)
+      CalculateAccesoryCostsForPEPU(PEPU_IndividualsList, InvestmentCategories, PEPU_Investment)
 
   PEPU_ReproductionAllocation:
     command: >
-      CalculateRAForPEPU(PEPU_Investment, PEPU_HarvestData, PEPU_GraphMaps, IndividualsList)
+      CalculateRAForPEPU(PEPU_Investment,  PEPU_HarvestData, PEPU_GraphMaps, PEPU_IndividualsList)
+
   PHPH_IndividualsList:
     command: filterToPHPH(IndividualsList)
 
@@ -684,11 +695,12 @@ targets:
 
   PHPH_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForPHPH(PHPH_ReproductionData, PHPH_HarvestData, InvestmentCategories, PHPH_Investment)
+      CalculateAccesoryCostsForPHPH(PHPH_IndividualsList, InvestmentCategories, PHPH_Investment)
 
   PHPH_ReproductionAllocation:
     command: >
-      CalculateRAForPHPH(PHPH_Investment, PHPH_HarvestData, PHPH_GraphMaps, IndividualsList)
+      CalculateRAForPHPH(PHPH_Investment,  PHPH_HarvestData, PHPH_GraphMaps, PHPH_IndividualsList)
+
   PILI_IndividualsList:
     command: filterToPILI(IndividualsList)
 
@@ -716,11 +728,12 @@ targets:
 
   PILI_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForPILI(PILI_ReproductionData, PILI_HarvestData, InvestmentCategories, PILI_Investment)
+      CalculateAccesoryCostsForPILI(PILI_IndividualsList, InvestmentCategories, PILI_Investment)
 
   PILI_ReproductionAllocation:
     command: >
-      CalculateRAForPILI(PILI_Investment, PILI_HarvestData, PILI_GraphMaps, IndividualsList)
+      CalculateRAForPILI(PILI_Investment,  PILI_HarvestData, PILI_GraphMaps, PILI_IndividualsList)
+
   PUTU_IndividualsList:
     command: filterToPUTU(IndividualsList)
 
@@ -748,11 +761,12 @@ targets:
 
   PUTU_AccessoryCosts:
     command: >
-      CalculateAccesoryCostsForPUTU(PUTU_ReproductionData, PUTU_HarvestData, InvestmentCategories, PUTU_Investment)
+      CalculateAccesoryCostsForPUTU(PUTU_IndividualsList, InvestmentCategories, PUTU_Investment)
 
   PUTU_ReproductionAllocation:
     command: >
-      CalculateRAForPUTU(PUTU_Investment, PUTU_HarvestData, PUTU_GraphMaps, IndividualsList)
+      CalculateRAForPUTU(PUTU_Investment,  PUTU_HarvestData, PUTU_GraphMaps, PUTU_IndividualsList)
+
   ReproductionAllocation_all:
     command: >
       combine_ReproductionAllocation(

commit 5c5a14fbe2a24e77656a18e8ee78cfc5cc546f8d

Closes #9 - accessory costs were empty
Bug introduced in 9d4ddf53927a86295dfa608566e412c7659acd9f

Also greatly simply function by siwtching over to dplyr syntax

R/AccessoryTissues.R
@@ -5,23 +5,15 @@ IAT_Species <- function(species, IndividualsList, InvestmentCategories, species_
     filter(use_for_allocation_calculations & alive) %>%
     select(individual, age)
 
-  InvDist <- data.frame(species=c(), individual = c(), PrePol_A = c(), PrePol_S = c(), PostPol_A = c(), PD = c(), Prop = c(), Total = c(), age = c(), ageExact = c())
-
-  for (individual in unique(IndividualsList$individual)) {
-    # print(individual)
-    Ind <- InvestmentInAccessoryTissues(individual, species, InvestmentCategories[, c("flower_part", species)], species_INV)
-    if (length(AgeData[AgeData[, 2] == individual, 1]) == 1) {
-      thisage <- AgeData[AgeData[, 2] == individual, 1]
-      Ind <- cbind(species, Ind, age=round(thisage, digits=1), age_exact = thisage)
-      InvDist <- rbind(InvDist, Ind)
-    }
-  }
+  InvDist <- lapply(unique(AgeData$individual), InvestmentInAccessoryTissues,
+                species=species, InvestmentCategories=InvestmentCategories[, c("flower_part", species)],
+                species_INV= species_INV) %>% rbind_all
 
 # TODO: This is temp fix to remove some NAs in PrePol_S. Figure out why these are needed
   InvDist$PrePol_S[which(is.na(InvDist$PrePol_S))] <- 0
   InvDist$Total= (InvDist$PrePol_A + InvDist$PrePol_S + InvDist$PostPol_A + InvDist$PD + InvDist$Prop)
 
-  InvDist
+  merge(AgeData, InvDist, by="individual")
 }
 
 
@@ -82,7 +74,7 @@ InvestmentInAccessoryTissues <- function(individual, species, InvestmentCategori
   }
   TotWeight <- PrepolinationAbortWeight + PrepolinationSuccessWeight + PostpollinationAbortWeight + PackaginDispersalWeight + PropaguleWeight
   data.frame(individual = individual, PrePol_A = PrepolinationAbortWeight, PrePol_S = PrepolinationSuccessWeight, PostPol_A = PostpollinationAbortWeight, PD = PackaginDispersalWeight,
-    Prop = PropaguleWeight, Total = TotWeight)
+    Prop = PropaguleWeight, Total = TotWeight, stringsAsFactors=FALSE)
 }
 
 TreeListToTotalDataFrame <- function(TreeList) {