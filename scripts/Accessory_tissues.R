4bcb42362c1dc586090b8a102d99df07815974be
Elizabeth Wenk <ehwenk@gmail.com>
2015-04-21 15:04:50 +1000

    work on accessory tissue analyses

--- scripts/Accessory_tissues.R
+++ scripts/Accessory_tissues.R
@@ -10,7 +10,38 @@
 
 #how can I tell R to figure out for itself the correct order to add variables to the model - depending on order, different variables become significant
 
-mod <- lm(asin(sqrt(RA))~asin(sqrt(PerPropagule))+asin(sqrt(PerPrePol_A))+ asin(sqrt(PerPrePol_S))+ asin(sqrt(PerPostPol_A))+ asin(sqrt(PerPackDisp)),SummaryInd)
+mod <- lm(log(Total)~asin(sqrt(PerPropagule))+asin(sqrt(PerPrePol_A))+ asin(sqrt(PerPackDisp))+ asin(sqrt(PerPrePol_S))+ asin(sqrt(PerPostPol_A)),data=subset(SummaryInd,RA>0))
+summary(mod)
+plot(mod)
+step(mod)
+hist(log(SummaryInd$Total))
+
+
+hist(SummaryInd$PerPropagule)
+hist(asin(sqrt(SummaryInd$PerPropagule)))
+
+hist(SummaryInd$PerPropagule)
+hist(asin(sqrt(SummaryInd$PerPropagule)))
+
+
+mod <- lm(log(Total)~asin(sqrt(PerAcc)),data=subset(SummaryInd,RA>0))
+summary(mod)
+#As Total investment in reproduction changes, effect of lots of reproductive components, but Propagule % (and hence total accesory %) doesn't matter
+Y<-cbind(asin(sqrt(SummaryInd$PerPropagule)),asin(sqrt(SummaryInd$PerPrePol_A)),asin(sqrt(SummaryInd$PerPackDisp)),asin(sqrt(SummaryInd$PerPrePol_S)),asin(sqrt(SummaryInd$PerPostPol_A)))
+mod <- lm(asin(sqrt(RA))~asin(sqrt(PerPropagule))+asin(sqrt(PerPrePol_A))+ asin(sqrt(PerPackDisp))+ asin(sqrt(PerPrePol_S))+ asin(sqrt(PerPostPol_A)),data=subset(SummaryInd,RA>0))
+
+mod <- manova(Y~asin(sqrt(RA))*log(Total),data=subset(SummaryInd,RA>0))
+summary(mod)
+summary.aov(mod)
+anova(mod)
+
+
+summary(mod)
+anova(mod)
+plot(mod)
+step(mod)
+
+mod <- lm(RA~PerPropagule+PerPrePol_A+ PerPrePol_S+ PerPostPol_A+ PerPackDisp,data=subset(SummaryInd,RA>0))
 summary(mod)
 plot(mod)
 step(mod)
@@ -30,6 +61,83 @@ summary(mod)
 mod <- lm(asin(sqrt(RA))~asin(sqrt(PerPropagule)),data=subset(SummaryInd,RA>0))
 summary(mod)
 
+plot(PerPrePol_A~PerPropagule,data=subset(SummaryInd,RA>0))
+plot(RA~PerPropagule,data=subset(SummaryInd,RA>0))
+
+#looking at shifts in allocation to different accessory categories with RA and age
+
+#shifts in pre-pollination abortion rates
+#Plants with higher RA have lower pre-pollination abortion
+plot(asin(sqrt(RA))~asin(sqrt(PerPrePol_A)),data=subset(SummaryInd,RA>0))
+mod <- lm(asin(sqrt(RA))~asin(sqrt(PerPrePol_A)),data=subset(SummaryInd,RA>0))
+summary(mod)
+
+#older plants have lower pre-pollination abortion
+plot(asin(sqrt(PerPrePol_A))~age,data=subset(SummaryInd,RA>0), log="x")
+mod <- lm(age~asin(sqrt(PerPrePol_A)),data=subset(SummaryInd,RA>0))
+summary(mod)
+
+#shifts in percent investment in propagules
+#Plants with higher RA invest propotionally more in propagules
+plot(asin(sqrt(RA))~asin(sqrt(PerPropagule)),data=subset(SummaryInd,RA>0))
+mod <- lm(asin(sqrt(RA))~asin(sqrt(PerPropagule))+species+species*asin(sqrt(PerPropagule)),data=subset(SummaryInd,RA>0))
+summary(mod)
+anova(mod)
+
+#investment in propagules does not shift with age
+plot(asin(sqrt(PerPropagule))~age,data=subset(SummaryInd,RA>0), log="x")
+mod <- lm(age~asin(sqrt(PerPropagule)),data=subset(SummaryInd,RA>0))
+summary(mod)
+
+#overall shifts in accessory costs - inverse of propagule relationships
+plot(asin(sqrt(RA))~asin(sqrt(PerAcc)),data=subset(SummaryInd,RA>0&age>2))
+mod <- lm(asin(sqrt(RA))~asin(sqrt(PerAcc))+species+asin(sqrt(PerAcc))*species,data=subset(SummaryInd,RA>0&age>2))
+summary(mod)
+#Higher RA = only a small shift in accessory costs, with no species*RA interactions
+
+mod_RA_by_Acc <- function(spp) {
+  plot(PerAcc ~ RA, data=subset(SummaryInd,RA>0 & species==spp),ylab="percent reproduction for accessory tissues",main=labels.spp[spp])
+}
+  
+  for(n in names(labels.spp)) {
+    mod_RA_by_Acc(n)
+  }
+
+#looking individually through species, only a handful of species have a significant relationship between RA and PerAcc, 
+#but always, with higher RA = lower PerAcc and a number of other species have p-value between 0.05 and 0.10.
+
+
+plot(PerAcc~age,data=subset(SummaryInd,RA>0&age>2), log="x")
+mod <- lm(PerAcc~age + species + age*species,data=subset(SummaryInd,RA>0&age>2))
+summary(mod)
+
+#full model indicates that for two species, accessory costs do decline with age:
+mod <- lm(PerAcc~age,data=subset(SummaryInd,RA>0 & species=="BOLE"&age>2))
+summary(mod)
+
+mod <- lm(PerAcc~age,data=subset(SummaryInd,RA>0 & species=="COER"))
+summary(mod)
+
+
+#once the effect of species is accounted for, height has no effect
+plot(asin(sqrt(PerAcc))~log(height),data=subset(SummaryInd,RA>0&age>2),log="x",col=col.spp[species])
+mod <- lm(asin(sqrt(PerAcc))~log(height)+species+log(height)*species,data=subset(SummaryInd,RA>0&age>2))
+summary(mod)
+hist(log(SummaryInd$height))
+
+plot(PerAcc~FinalWeight,data=subset(SummaryInd,RA>0&age>2),log="x",col=col.spp[species])
+mod <- lm(PerAcc~FinalWeight+species+height*species,data=subset(SummaryInd,RA>0&age>2))
+summary(mod)
+
+plot(seed_size~PerAcc_mean,SummarySpp,log="xy")
+mod <- lm(seed_size~PerAcc_mean,SummarySpp)
+summary(mod)
+#species with larger seed have higher accessory costs
+
+plot(seed_size~PerAcc_mean,data=subset(SummarySppAge,age>2.4),log="xy")
+mod <- lm(PerAcc_mean~seed_size+age+seed_size*age,data=subset(SummarySppAge,age>2.4))
+summary(mod)
+#species with larger seed have higher accessory costs
 
 
 plot(PerPrePol_A~PerPropagule,data=subset(SummaryInd,RA>0))
@@ -88,4 +196,16 @@ legend("bottomleft",col=col.spp, labels.spp, pch=16, cex=1)
 
 mod <- lm(log(FinalWeight)~ asin(sqrt(PerPropagule)) + species,SummaryInd)
 summary(mod)
+
+
+repro <- c("Propagules", "Aborted pre-pollination","Successful pre-pollination","Aborted post-pollination","Packaging and dispersal")
+pch.repro <- c(16,17,18,4,5)
+plot(PerPropagule_mean~species,SummarySpp,ylab="percent total reproductive investment",cex.axis=.8,ylim=c(0,1), pch=16)
+legend("topright",repro,pch=pch.repro)
+points(PerPrePol_A_mean~species,SummarySpp, pch=17)
+points(PerPrePol_S_mean~species,SummarySpp, pch=18)
+points(PerPostPol_A_mean~species,SummarySpp, pch=4)
+points(PerPackDisp_mean~species,SummarySpp, pch=5)
+
+
 
