44efee53704e83c01f1d02ee2b103bb52b9eac92
Daniel Falster <daniel.falster@mq.edu.au>
2015-08-14 06:57:01 +1000

    process leavesperlenght and leaf lifespan

--- R/Summaries.R
+++ R/Summaries.R
@@ -47,3 +47,63 @@ process_LMA <- function(LMA_raw) {
   LMA$leaf_size <- LMA$leaf_area/LMA$leaf_number
   LMA
 }
+
+process_leaves_per_length <- function(leavesPerLength_raw) {
+    # get average leaves per length for spp
+  filter(leavesPerLength_raw,use=="yes") %>%
+    mutate(count_per_length = leaf_count/length_mm) %>%
+    group_by(species) %>%
+    summarise(count_per_length = mean(count_per_length, na.rm=TRUE))
+}
+
+process_leaf_lifespan <- function(leafLifespan_raw, leavesPerLength) {
+  leafLifespan <- filter(leafLifespan_raw, dont_use!="dead" & dont_use!="dont_use") %>%
+    select(species,age,individual,d_start,d_end,length_start,new_length,lvs_start_length,lvs_start_count,lvs_end_length,lvs_end_count,lvs_new_length,lvs_new_count,new_but_shed_count,mm_lvs_spec,count_lvs_spec)
+
+  leafLifespan$individual <- as.factor(leafLifespan$individual)
+  leafLifespan$species <- as.factor(leafLifespan$species)
+
+  #loading this column as character, so using this for now
+
+  # Calculating Leaf Lifespan
+
+  # where counts did not previously exists, set as 0; CHECK WITH DANIEL THAT "V" IS A SET "NAME" IN THESE FUNCTIONS
+
+  leaf_lifespan_temp <- leafLifespan %>%
+    select(species,age,individual,d_start,d_end,length_start,new_length,lvs_start_count,lvs_end_count,lvs_new_count,new_but_shed_count)
+
+  for(v in c("lvs_start_count","lvs_end_count","lvs_new_count","new_but_shed_count")) {
+    i <- is.na(leaf_lifespan_temp[[v]])
+    leaf_lifespan_temp[[v]][i] <- 0
+  }
+
+  # For individuals with "length" measures, translate into count
+
+
+  # get spp average for all individuals
+  leafLifespan$count_per_length <- leavesPerLength$count_per_length[match(leafLifespan$species, leavesPerLength$species)]
+
+  # if possible use data for that individual, if it exists; this code substitutes in species count per length #s for individuals where this data is available
+  i <- !is.na(leafLifespan$mm_lvs)
+  leafLifespan$count_per_length[i] <- (leafLifespan$count_lvs_spec/leafLifespan$mm_lvs)[i]
+
+  #this is the end of the section Daniel helped me rewrite - next section is mine
+
+  #get total leaf counts
+  for(v in c("lvs_start_length","lvs_start_count","lvs_end_length","lvs_end_count","lvs_new_length","lvs_new_count","new_but_shed_count")) {
+    i <- is.na(leafLifespan[[v]])
+    leafLifespan[[v]][i] <- 0
+  }
+  leafLifespan$lvs_start <- leafLifespan$lvs_start_count + (leafLifespan$lvs_start_length * leafLifespan$count_per_length)
+  leafLifespan$lvs_end <- leafLifespan$lvs_end_count + (leafLifespan$lvs_end_length * leafLifespan$count_per_length)
+  leafLifespan$lvs_new <- leafLifespan$lvs_new_count + (leafLifespan$lvs_new_length * leafLifespan$count_per_length)
+
+  #calculating leaf lifespan
+  leafLifespan$LL_bd <- (leafLifespan$lvs_start)/(((leafLifespan$lvs_start)-(leafLifespan$lvs_end))+(leafLifespan$lvs_new)/2)
+  leafLifespan$LL_death <- (leafLifespan$lvs_start)/((leafLifespan$lvs_start)-(leafLifespan$lvs_end))
+  leafLifespan$LL_birth <- (leafLifespan$lvs_start)/(leafLifespan$lvs_new)
+
+  leafLifespan$LL_death[which(is.infinite(leafLifespan$LL_death))] <- NA
+  leafLifespan$LL_bd[which(is.infinite(leafLifespan$LL_death))] <- NA
+  leafLifespan$LL_birth[which(is.infinite(leafLifespan$LL_death))] <- NA
+}
--- scripts/summary_dataframes.R
+++ scripts/summary_dataframes.R
@@ -11,61 +11,6 @@ investment$age <- round(investment$age, digits=1)
 
 accessory <- AccessoryCosts_all
 
-leafLifespan <- filter(leafLifespan_raw, dont_use!="dead" & dont_use!="dont_use") %>%
-  select(species,age,individual,d_start,d_end,length_start,new_length,lvs_start_length,lvs_start_count,lvs_end_length,lvs_end_count,lvs_new_length,lvs_new_count,new_but_shed_count,mm_lvs_spec,count_lvs_spec)
-
-leafLifespan$individual <- as.factor(leafLifespan$individual)
-leafLifespan$species <- as.factor(leafLifespan$species)
-
-#loading this column as character, so using this for now
-
-# Calculating Leaf Lifespan
-
-# where counts did not previously exists, set as 0; CHECK WITH DANIEL THAT "V" IS A SET "NAME" IN THESE FUNCTIONS
-
-leaf_lifespan_temp <- leafLifespan %>%
-  select(species,age,individual,d_start,d_end,length_start,new_length,lvs_start_count,lvs_end_count,lvs_new_count,new_but_shed_count)
-
-for(v in c("lvs_start_count","lvs_end_count","lvs_new_count","new_but_shed_count")) {
-  i <- is.na(leaf_lifespan_temp[[v]])
-  leaf_lifespan_temp[[v]][i] <- 0
-}
-
-# For individuals with "length" measures, translate into count
-
-# get average leaves per length for spp
-leaves_per_length_spp <-  filter(leavesPerLength_raw,use=="yes") %>%
-  mutate(count_per_length = leaf_count/length_mm) %>%
-  group_by(species) %>%
-  summarise(count_per_length = mean(count_per_length, na.rm=TRUE))
-
-# get spp average for all individuals
-leafLifespan$count_per_length <- leaves_per_length_spp$count_per_length[match(leafLifespan$species, leaves_per_length_spp$species)]
-
-# if possible use data for that individual, if it exists; this code substitutes in species count per length #s for individuals where this data is available
-i <- !is.na(leafLifespan$mm_lvs)
-leafLifespan$count_per_length[i] <- (leafLifespan$count_lvs_spec/leafLifespan$mm_lvs)[i]
-
-#this is the end of the section Daniel helped me rewrite - next section is mine
-
-#get total leaf counts
-for(v in c("lvs_start_length","lvs_start_count","lvs_end_length","lvs_end_count","lvs_new_length","lvs_new_count","new_but_shed_count")) {
-  i <- is.na(leafLifespan[[v]])
-  leafLifespan[[v]][i] <- 0
-}
-leafLifespan$lvs_start <- leafLifespan$lvs_start_count + (leafLifespan$lvs_start_length * leafLifespan$count_per_length)
-leafLifespan$lvs_end <- leafLifespan$lvs_end_count + (leafLifespan$lvs_end_length * leafLifespan$count_per_length)
-leafLifespan$lvs_new <- leafLifespan$lvs_new_count + (leafLifespan$lvs_new_length * leafLifespan$count_per_length)
-
-#calculating leaf lifespan
-leafLifespan$LL_bd <- (leafLifespan$lvs_start)/(((leafLifespan$lvs_start)-(leafLifespan$lvs_end))+(leafLifespan$lvs_new)/2)
-leafLifespan$LL_death <- (leafLifespan$lvs_start)/((leafLifespan$lvs_start)-(leafLifespan$lvs_end))
-leafLifespan$LL_birth <- (leafLifespan$lvs_start)/(leafLifespan$lvs_new)
-
-leafLifespan$LL_death[which(is.infinite(leafLifespan$LL_death))] <- NA
-leafLifespan$LL_bd[which(is.infinite(leafLifespan$LL_death))] <- NA
-leafLifespan$LL_birth[which(is.infinite(leafLifespan$LL_death))] <- NA
-
 #calculating percentages of various accessory tissues
 
 accessory$accessory_inv <- accessory$total_repro_inv - accessory$propagule_inv
