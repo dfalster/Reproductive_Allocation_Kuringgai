f699965a2164a2a2fb062637432e63115b982480
Elizabeth Wenk <ehwenk@gmail.com>
2015-08-20 17:05:25 +1000

    deleting unneeded files

--- Panalysis.R
+++
@@ -1,7 +0,0 @@
-
-
-```{r, echo=FALSE,results='hide', message=FALSE}
-plot((1+propagule_inv)~(1+prepollen_all_inv),SummaryInd, col=col.spp(species), pch=16, log="xy",ylab="investment in propagules (mg)",xlab="investment in all prepollination tissues (mg)")
-abline(0,1)
-legend("topleft",legend=labels.spp(),col=col.spp,pch=16, cex=.8,bty="n")
-```{r, echo=FALSE,results='hide', message=FALSE}
