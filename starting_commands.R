477091c77c502f6fd2808894f3bc2d1b5b110a00
Daniel Falster <daniel.falster@mq.edu.au>
2015-08-25 09:04:48 +1000

    Make export step, this will make it easier for Lizzy to run knitr scripts without getting remake involved

--- starting_commands.R
+++ starting_commands.R
@@ -1,5 +1,7 @@
-library(remake)
-make("SummaryInd")
-make("SummarySpp")
-make("SummarySppAge")
-create_bindings()
+remake::make("export")
+
+SummaryInd <- readRDS("export/SummaryInd.rds")
+SummarySpp <- readRDS("export/SummarySpp.rds")
+SummarySppAge <- readRDS("export/SummarySppAge.rds")
+
+source("R/figures.R")
