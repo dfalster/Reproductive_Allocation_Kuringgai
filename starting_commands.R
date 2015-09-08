d0a895eecf00483b5045259e292d2f5319aa96d4
Elizabeth Wenk <ehwenk@gmail.com>
2015-09-08 17:13:12 +1000

    adding two new dataframes to be read

--- starting_commands.R
+++ starting_commands.R
@@ -1,7 +1,10 @@
 remake::make("export")
 
+
 SummaryInd <- readRDS("export/SummaryInd.rds")
 SummarySpp <- readRDS("export/SummarySpp.rds")
 SummarySppAge <- readRDS("export/SummarySppAge.rds")
+HarvestData <- readRDS("export/HarvestData.rds")
+InvestmentByPart <- readRDS("export/Investment_FD_all.rds")
 
 source("R/figures.R")
