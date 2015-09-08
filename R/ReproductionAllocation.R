a847124caef0d221e08b4165ed2da57dddbd14cb
Elizabeth Wenk <ehwenk@gmail.com>
2015-09-08 17:12:15 +1000

    starting to fix problems with leaf weight estimates

--- R/ReproductionAllocation.R
+++ R/ReproductionAllocation.R
@@ -1,3 +1,4 @@
+
 RA_Calculations <- function(thisSpecies, Species_Investment, HarvestData, Maps, IndividualsList) {
 
   individuals <- filter(IndividualsList, use_for_allocation_calculations & alive)$individual
@@ -11,20 +12,20 @@ RA_Calculations <- function(thisSpecies, Species_Investment, HarvestData, Maps, 
     lm(log(data[[var]]) ~ log(data[["diameter"]]))
   }
 
-  fit_total <- lm.fit("total_weight", HarvestData_basal_end)
+  fit_leaf <- lm.fit("leaf_weight", HarvestData_basal_end)
   fit_stem <- lm.fit("stem_weight", HarvestData_basal_end)
 
   HarvestData_basal_end <- HarvestData_basal_end %>%
     mutate(
-      slope_total  = as.numeric(coef( fit_total)[2]),
-      intercept_total = log(total_weight) - slope_total * log(diameter),
+      slope_leaf  = as.numeric(coef( fit_leaf)[2]),
+      intercept_leaf = log(leaf_weight) - slope_leaf * log(diameter),
       slope_stem  = as.numeric(coef( fit_stem)[2]),
       intercept_stem = log(stem_weight) - slope_stem * log(diameter))
 
 
-  predict_total_weight <- function(diameter, thisindividual) {
+  predict_leaf_weight <- function(diameter, thisindividual) {
     x <- HarvestData_basal_end %>% filter(individual==thisindividual)
-    exp(x$intercept_total + x$slope_total * log(diameter))
+    exp(x$intercept_leaf + x$slope_leaf * log(diameter))
   }
 
   predict_stem_weight <- function(diameter, thisindividual) {
@@ -35,34 +36,16 @@ RA_Calculations <- function(thisSpecies, Species_Investment, HarvestData, Maps, 
   ## Estimate weight at beginning and end  (year 2012 and 2013) using fitted regression
   GrowthEst <- HarvestData_basal %>%
     group_by(individual) %>%
-    mutate(total_weight_est =  predict_total_weight(diameter, individual[1]),
-           growth_inv = c(NA, diff(total_weight_est)),
+    mutate(
+           leaf_weight_est =  predict_leaf_weight(diameter, individual[1]),
+           growth_leaf = c(NA, diff(leaf_weight_est)),
            stem_weight_est = predict_stem_weight(diameter, individual[1]),
-           growth_stem = c(NA, diff(stem_weight_est)),
-           growth_leaf =  growth_inv - growth_stem,
+           growth_stem = c(NA, diff(stem_weight_est)),           
+           growth_inv =  growth_leaf + growth_stem,
            growth_height = c(NA, diff(height)),
            growth_stem_diameter = c(NA, diff(diameter)),
            growth_stem_area = c(NA, diff(stem_area))
            ) %>%
     filter(start_end == "end") %>%
     select(species, site, individual, age, height, diameter, stem_area, leaf_weight, stem_weight, total_weight, growth_inv, growth_stem, growth_leaf, growth_height, growth_stem_diameter, growth_stem_area)
-
-#     # Use saved data to calculate total reproduction investment per individual plant
-#     ReproTotal <- Species_Investment$Investment %>%
-#      group_by(individual) %>%
-#      summarise(
-#       repro_inv = sum(Total)
-#       )
-# 
-#   # Merge two tables and calculate total investment and RAR
-#   InvestmentSummary <- merge(ReproTotal, GrowthEst, by = "individual", all.y = TRUE)
-#   # NA that appeared correspond to zero reproductive investment
-#   InvestmentSummary[is.na(InvestmentSummary)] <- 0
-# 
-#   browser()
-#   InvestmentSummary %>%
-#     mutate(
-#       total_inv = repro_inv + growth_inv,
-#       RA = repro_inv/total_inv)
-# InvestmentSummary <- GrowthEst
 }
