81b202dd7256ffde7478ca2decf3f80e9589d81f
Elizabeth Wenk <ehwenk@gmail.com>
2016-06-20 16:15:08 +1000

    edits to plots looking at RA, leaf growth with new model fits

--- scripts/checking_new_leaf_growth.R
+++ scripts/checking_new_leaf_growth.R
@@ -1,9 +1,9 @@
-pdf("figs2/new_leaf_growth.pdf", height=11.69, width=8.27)
+pdf("figs2/new_leaf_growth.pdf", height=8.27, width=11.69)
 plot
 
 par(mfrow=c(2,2), cex=1, omi=c(.1,.1,.1,.1), mai=c(1.2,1.1,.4,0.2))
 
-data <- SummaryInd
+data <- subset(SummaryInd,age>1)
 data <- split(data, data$species)
 
 for(spp in names(data)) {
@@ -12,26 +12,25 @@ data[[spp]]$age_0 = data[[spp]]$age-1
 data[[spp]]$growth_leaf_adjust <- data[[spp]]$growth_leaf - min(data[[spp]]$growth_leaf) +1
 data2 <- subset(SummarySppAge[["mean"]],species==spp)
 
-plot(leaf_area~diameter,data[[spp]],pch=16,col=col.age(age),log="xy",ylim=c(1,max(leaf_area*1.1)),xlim=c(min(diameter_0*0.9),max(diameter*1.1)))
+plot(leaf_area~diameter,data[[spp]],pch=16,col=col.age(age),log="xy",ylim=c(1,max((data[[spp]]$leaf_area*1.1))),xlim=c(min((data[[spp]]$diameter_0*0.9))+1,max(data[[spp]]$diameter*1.1)))
 points(leaf_adjust~diameter_0,data[[spp]],pch=1,col=col.age(age))
 arrows(data[[spp]]$diameter_0,data[[spp]]$leaf_adjust,data[[spp]]$diameter,data[[spp]]$leaf_area,code=0,length=0,col=col.age(data[[spp]]$age))
-abline(log10(min(data[[spp]]$leaf_area_0)),0)
+abline(log10(min(data[[spp]]$leaf_area_0)+1),0)
 title(main=spp)
 
-plot(leaf_area~age,data[[spp]],pch=16,col=col.age(age),log="xy",ylim=c(1,max(leaf_area*1.1)),xlim=c(0.3,max(age)+1))
+plot(leaf_area~age,data[[spp]],pch=16,col=col.age(age),log="y",ylim=c(1,max((data[[spp]]$leaf_area*1.1))),xlim=c(0.3,max(data[[spp]]$age)+1))
 points(leaf_adjust~age_0,data[[spp]],pch=1,col=col.age(age))
 arrows(data[[spp]]$age_0,data[[spp]]$leaf_adjust,data[[spp]]$age,data[[spp]]$leaf_area,code=0,length=0,col=col.age(data[[spp]]$age))
-abline(log10(min(data[[spp]]$leaf_area_0)),0)
+abline(log10(min(data[[spp]]$leaf_area_0)+1),0)
 title(main=spp)
 
 
-#plot(growth_leaf_adjust~repro_inv,data[[spp]],pch=16,col=col.age(age),log="xy")
-if (min(data[[spp]]$growth_leaf) < 0) {
-  plot(growth_leaf_adjust~repro_inv,data[[spp]],pch=16,col=col.age(age),log="xy")
-  abline(log10(-min(data[[spp]]$growth_leaf)),0)
+if (min((data[[spp]]$growth_leaf)) <= 0) {
+  plot(growth_leaf~repro_inv,data[[spp]],pch=16,col=col.age(age),log="x")
+  abline((-min((data[[spp]]$growth_leaf))),0)
 } else {
   plot(growth_leaf~repro_inv,data[[spp]],pch=16,col=col.age(age),log="xy")
-  abline(log10(min(data[[spp]]$growth_leaf)),0)
+  abline(log10(min((data[[spp]]$growth_leaf))),0)
   }
 title(main=spp)
 
@@ -42,3 +41,4 @@ title(main=spp)
 }
 
 dev.off()
+
